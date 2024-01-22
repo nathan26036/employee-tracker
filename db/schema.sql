99ec61eb952b514de8b1502e52e4cbbaf04c2393
nathan26036 <nathandickinson04@gmail.com>
2024-01-22 00:24:45 -0500

    Created the questions and created ifs for each question that will show the user what they have selectedand fixed the db schema file and added sequelize npm

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,23 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE departnment (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL TEXT NOT NULL,
-    departnment_id INT NOT NULL,
-    FOREIGN KEY (departnment_id)
-    REFERENCES departnment(id)
-    ON DELETE SET NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT NOT NULL
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL,
-    manager_id INT NOT NULL
-    REFERENCES employee(id)
-    ON DELETE SET NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT
 );
 
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,18 @@
-INSERT INTO departnment ()
-VALUES 
+INSERT INTO department (name)
+VALUES   ('Engineering'),
+         ('Sales'),
+         ('Finance'),
+         ('Legal');
 
-INSERT INTO role ()
-VALUES 
+
+INSERT INTO role (title, salary, department_id)
+VALUES   ('Software Engineer', 85000, 1),
+         ('Salesperson', 75000, 2),
+         ('Accountant', 125000, 3),
+         ('Lawyer', 200000, 4);
        
-INSERT INTO employee ()
-VALUES 
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES   ('John', 'Doe', 1, 4),
+         ('Jonathan', 'Doeth', 2, 3),
+         ('Jill', 'Jackson', 3, 1),
+         ('Ernie', 'Bernie', 4, 5);
